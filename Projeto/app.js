097442fe3ad1d1711d0d563b9995857b7a4ae757
Tulio Barroso <tuliobscorreia15@gmail.com>
2024-09-04 13:05:02 -0300

    Update 5 DADOS.js
    Criação de um arquivo js  (dados.js) para armazenar os dados de forma organizada

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,1 +1,2 @@
-console.log("Hello World!")
+console.log(dados)   //Quiser acessar um especifico: dados[2]
+
---
+++ Projeto/dados.js
@@ -0,0 +1,10 @@
+//arquivo js para amarzenar alguns dados.
+
+let dados = ["Lewis Hamilton" ,"Valtteri Bottas", "Max Verstappen"];  //[ARRAY]
+
+let lewis = {   //{OBJETO}
+        titulo : "Lewis Hamilton",
+        descricao : "Lewis Hamilton é um dos maiores pilotos de Fórmula 1 de todos os tempos. Com sete títulos mundiais, ele se tornou uma lenda do esporte e um ícone da cultura pop. Além de suas conquistas nas pistas, Hamilton é conhecido por sua personalidade carismática e por ser um defensor de causas sociais importantes. Sua trajetória na Fórmula 1, marcada por vitórias memoráveis e rivalidades intensas, o transformou em um dos atletas mais admirados do mundo.",
+        link : "https://pt.wikipedia.org/wiki/Lewis_Hamilton",
+        social : "https://www.instagram.com/lewishamilton"
+}
