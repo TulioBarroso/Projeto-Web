c5459cc350d97b0eb862db0754ef97c80df6d168
Tulio Barroso <tuliobscorreia15@gmail.com>
2024-09-04 13:18:50 -0300

    Update 6 DADOS.js
    criei objeto para os pilotos e coloquei dentro de um array

--- Projeto/dados.js
+++ Projeto/dados.js
@@ -1,10 +1,26 @@
 //arquivo js para amarzenar alguns dados.
 
-let dados = ["Lewis Hamilton" ,"Valtteri Bottas", "Max Verstappen"];  //[ARRAY]
+let dados = [ //[ARRAY] - coloquei um objeto dentro do array //  ARRAY entre [] e ,
 
-let lewis = {   //{OBJETO}
-        titulo : "Lewis Hamilton",
-        descricao : "Lewis Hamilton é um dos maiores pilotos de Fórmula 1 de todos os tempos. Com sete títulos mundiais, ele se tornou uma lenda do esporte e um ícone da cultura pop. Além de suas conquistas nas pistas, Hamilton é conhecido por sua personalidade carismática e por ser um defensor de causas sociais importantes. Sua trajetória na Fórmula 1, marcada por vitórias memoráveis e rivalidades intensas, o transformou em um dos atletas mais admirados do mundo.",
-        link : "https://pt.wikipedia.org/wiki/Lewis_Hamilton",
-        social : "https://www.instagram.com/lewishamilton"
-}
+    {   //{OBJETO}  objeto entre {} / nomedavariavel : dado,  
+    titulo : "Lewis Hamilton",
+    descricao : "Lewis Hamilton é um dos maiores pilotos de Fórmula 1 de todos os tempos. Com sete títulos mundiais, ele se tornou uma lenda do esporte e um ícone da cultura pop. Além de suas conquistas nas pistas, Hamilton é conhecido por sua personalidade carismática e por ser um defensor de causas sociais importantes. Sua trajetória na Fórmula 1, marcada por vitórias memoráveis e rivalidades intensas, o transformou em um dos atletas mais admirados do mundo.",
+    link : "https://pt.wikipedia.org/wiki/Lewis_Hamilton",
+    social : "https://www.instagram.com/lewishamilton"
+},
+{
+    titulo: "Valtteri Bottas",
+    descricao: "Valtteri Bottas é um piloto de Fórmula 1 finlandês que conquistou diversos pódios e vitórias na categoria. Conhecido por sua consistência e habilidade em ultrapassagens, Bottas foi companheiro de equipe de Lewis Hamilton na Mercedes por várias temporadas. Sua experiência e profissionalismo o tornaram um dos pilotos mais respeitados do grid.",
+    link: "https://pt.wikipedia.org/wiki/Valtteri_Bottas",
+    social: "https://www.instagram.com/valtteribottas"
+  },
+  {
+    titulo: "Max Verstappen",
+    descricao: "Max Verstappen é um piloto de Fórmula 1 holandês que se destacou desde sua estreia na categoria. Conhecido por seu estilo de pilotagem agressivo e talento natural, Verstappen conquistou seu primeiro título mundial em 2021, após uma intensa disputa com Lewis Hamilton. Sua juventude e ambição o colocam como um dos grandes nomes do futuro da Fórmula 1.",
+    link: "https://pt.wikipedia.org/wiki/Max_Verstappen",
+    social: "https://www.instagram.com/maxverstappen1"
+  }
+
+
+];  
+
