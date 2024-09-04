c83ba6de4b4cc3c50882bf1ec56e72b1a44a45db
Tulio Barroso <tuliobscorreia15@gmail.com>
2024-09-04 13:36:13 -0300

    Update 7 APP.js
    variações do console.log

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,2 +1,3 @@
-console.log(dados)   //Quiser acessar um especifico: dados[2]
+console.log(dados);   //Quiser acessar um especifico no array: dados[2]
 
+console.log(dados[0].link); //Uso do ponto( . ) para acessar alguma variavel especifica
