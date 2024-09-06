7e5ba9e6476190a7877fc27bf5888a2cc91f0c96
Tulio Barroso <tuliobscorreia15@gmail.com>
2024-09-06 15:24:21 -0300

    Update 12.1
    Criei um if para relacionar oque escrevo no campo de pesquisa está nos meus dados, se tiver, então mostra resultado

--- Projeto/app.js
+++ Projeto/app.js
@@ -36,17 +36,24 @@ let resultados = " "
 
 // jeito com repetição  
 for(let dado of dados){
-    resultados += `     
-    <div class="item-resultado">
-                    <h2>${dado.titulo}</h2> 
-                    <p class="descricao-meta">${dado.descricao}
-                    </p>
-                    <a href="${dado.link}" target="_blank">Mais informações</a>      
-            </div>`
+        //se titulo includes campoPesquisa(oque digitou)
+        if(dado.titulo.includes(campoPesquisa)){
+                
+                resultados += `     
+                <div class="item-resultado">
+                                <h2>${dado.titulo}</h2> 
+                                <p class="descricao-meta">${dado.descricao}
+                                </p>
+                                <a href="${dado.link}" target="_blank">Mais informações</a>      
+                        </div>`      
+
+        }
+        
+    
 }                    
 
 
-section.innerHTML = resultados
+section.innerHTML = resultados;
 
 
  }
